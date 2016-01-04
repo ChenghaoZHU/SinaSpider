9f5fff4421fe419d27f5c57046572cc93b5ab2df
Suda_Cat <zhu16814@163.com>
2016-01-04 16:50:33 +0800

    replace /001 in text of timelines

--- Test.py
+++ Test.py
@@ -1,3 +1,4 @@
+#-*- coding: UTF-8 -*-
 __author__ = 'chzhu'
 
 import Spider
@@ -6,14 +7,19 @@ from Dao import Task
 
 if __name__ == '__main__':
 
-    user_1 = Spider.User('shimiaohuirv1203@163.com', 'a123456')
-    user_2 = Spider.User('1352875156@qq.com', 'w1127401044')
-    user_list = [user_1, user_2]
+    # user_1 = Spider.User('shimiaohuirv1203@163.com', 'a123456')
+    # user_2 = Spider.User('1352875156@qq.com', 'w1127401044')
+    # user_list = [user_1, user_2]
+    #
+    # sp = SP(user_list)
+    #
+    # uid_list = Task.get_all()
+    #
+    # for uid in uid_list:
+    #     sp.collect_user_profiles_only(uid)
+    #     sp.save_only_profile()
 
-    sp = SP(user_list)
-
-    uid_list = Task.get_all()
-
-    for uid in uid_list:
-        sp.collect_user_profiles_only(uid)
-        sp.save_only_profile()
+    text = '哈哈哈'
+    print text
+    text = text.replace('', '')
+    print text
