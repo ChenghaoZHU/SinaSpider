e4bc97737d30b92fc995c3f7e494d255ca64fc34
Suda_Cat <zhu16814@163.com>
2016-01-28 10:39:26 +0800

    adas

---
+++ Config.py
@@ -0,0 +1,32 @@
+__author__ = 'chzhu'
+
+LOG_FILE = 'Log/log.txt'
+
+SLEEP_BETWEEN_2FPAGES = 5 # also for parts in one timeline page
+SLEEP_BETWEEN_TIMELINE_PAGES = 30
+SLEEP_WHEN_EXCEPTION = 60
+
+ACCOUNT_CHANGE_TIME = 30*60
+
+TABLES = {
+    'followee':'weibo_followees',
+    'follower':'weibo_followers',
+    'timeline':'weibo_timelines',
+    'job':'weibo_jobs',
+    'education':'weibo_educations',
+    'user':'weibo_users',
+    'account':'weibo_accounts',
+    'task':'weibo_tasks',
+    'parameter':'weibo_parameters'
+}
+
+DB_USER = 'root'
+DB_PASSWD = 'admin'
+DB_HOST = '127.0.0.1'
+DB_DATABASE = 'sina_weibo'
+DB_CHARSET = 'utf8mb4'
+
+ACCOUNT_NUM = 3
+TASK_NUM = 100
+
+OS = 0 # 0 is for Windows, 1 is for Linux
