881f80402647566850b08d5ee37dc73e5ba51fdd
Suda_Cat <zhu16814@163.com>
2016-01-31 19:16:40 +0800

    das

--- Config.py
+++ Config.py
@@ -2,8 +2,8 @@ __author__ = 'chzhu'
 
 LOG_FILE = 'Log/log.txt'
 
-SLEEP_BETWEEN_2FPAGES = 5 # also for parts in one timeline page
-SLEEP_BETWEEN_TIMELINE_PAGES = 30
+SLEEP_BETWEEN_2FPAGES = 15 # also for parts in one timeline page
+SLEEP_BETWEEN_TIMELINE_PAGES = 45
 SLEEP_WHEN_EXCEPTION = 60
 
 ACCOUNT_CHANGE_TIME = 30*60
@@ -26,7 +26,7 @@ DB_HOST = '127.0.0.1'
 DB_DATABASE = 'sina_weibo'
 DB_CHARSET = 'utf8mb4'
 
-ACCOUNT_NUM = 1
+ACCOUNT_NUM = 3
 TASK_NUM = 100
 
-OS = 0 # 0 is for Windows, 1 is for Linux
+OS = 1 # 0 is for Windows, 1 is for Linux
